ae23615c7e10f11d60c037d0101b5f209eea381e
Jacob Gutierrez <jacobgtzmtz@gmail.com>
2026-04-16 23:25:01 -0600

    First Configuration

---
+++ Seccion2/Variables/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("--- Sección 2: Variables en C# ---");
+
+string nombreCurso = "Programando en C#";
+int cantidadSecciones = 2;
+
+Console.WriteLine($"Bienvenido al curso {nombreCurso}. Actualmente estamos en la sección {cantidadSecciones}.");
